19ed57f19e54d3c87399cd00736818c89701c359
Abin Raj <abinraj.mk@vonnue.com>
2026-06-22 14:10:27 +0530

    feat: logged the  type of each primitive type

---
+++ js/week1/types.js
@@ -0,0 +1,33 @@
+console.log(typeof(null))
+// prediction : object 
+
+console.log(typeof(1))
+// prediction : number
+
+console.log(typeof(NaN))
+// prediction :  NaN , output : number 
+// reason : Nan stands for 'not a number' - its a special numberic data type
+
+console.log(typeof('s'))
+// prediction : string
+
+console.log(typeof(true))
+// pediction : boolean 
+
+console.log(typeof(0.014))
+// prediction : float
+
+console.log(typeof(NaN==NaN))
+//prediction : boolean
+
+console.log(typeof(undefined))
+//prediction : undefined
+
+console.log(typeof(function(){}))
+//prediction : function
+
+console.log(typeof([]))
+//prediction : object
+
+console.log(typeof(11n))
+// prediction : bigint

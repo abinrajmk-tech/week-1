156403ceda290521c4a577d70fb1ffeb09d55986
Abin Raj <abinraj.mk@vonnue.com>
2026-06-23 16:46:29 +0530

    feat: built mobile navigation drawer
    - clicking the hamburger adds class 'open' to the drawer.
    - implemented a focus trap.
    - pressing escape closes the drawer.

---
+++ js/week1/project/nav.js
@@ -0,0 +1,45 @@
+const hamburgerButton = document.querySelector(".hamburger");
+const navbar = document.querySelector(".sidebar-nav");
+// navbar.style.height="200px"
+navbar.tabIndex = -1;
+
+let isOpen = false;
+
+hamburgerButton.addEventListener("click", () => {
+    if (!isOpen) {
+        navbar.classList.add("open");
+        navbar.setAttribute("aria-expanded", true);
+    } else {
+        navbar.classList.remove("open");
+        navbar.setAttribute("aria-expanded", false);
+    }
+    isOpen = !isOpen;
+    navbar.focus();
+});
+navbar.addEventListener("keydown", (event) => {
+    if (event.key == "Escape" && isOpen)
+        { isOpen = !isOpen;
+    navbar.classList.remove("open");
+        }
+});
+
+navbar.addEventListener("keydown", (event) => {
+    let query = ".sidebar-nav a";
+    let focusable = document.querySelectorAll(query);
+    let first = focusable[0];
+    let last = focusable[focusable.length - 1];
+
+    if (event.key === "Tab") {
+        if (event.shiftKey) {
+            if (document.activeElement === first) {
+                last.focus();
+                event.preventDefault();
+            }
+        } else {
+            if (document.activeElement === last) {
+                first.focus();
+                event.preventDefault();
+            }
+        }
+    }
+});

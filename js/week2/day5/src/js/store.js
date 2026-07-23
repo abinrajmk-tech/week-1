7ce34595a8b1df2aa1525bbe7aed3dfa3bf29908
Abin Raj <abinraj.mk@vonnue.com>
2026-07-23 14:29:40 +0530

    feat: Built router and state manager

---
+++ js/week2/day5/src/js/store.js
@@ -0,0 +1,113 @@
+// store.js
+export const initialState = {
+    route: {
+        path: "/",
+        params: {},
+    },
+    tasks: [
+        {
+            id: 0,
+            title: "Create Login Page",
+            assigned: "Rahul",
+            due: "25-AUG-2026",
+            status: "Pending",
+            priority: "High",
+        },
+        {
+            id: 1,
+            title: "Design Layout",
+            assigned: "Priya",
+            due: "30-Jul-2026",
+            status: "Completed",
+            priority: "Medium",
+        },
+        {
+            id: 2,
+            title: "API Integration",
+            assigned: "Arjun",
+            due: "10-Sep-2026",
+            status: "Pending",
+            priority: "High",
+        },
+        {
+            id: 3,
+            title: "Write Unit Tests",
+            assigned: "Sneha",
+            due: "5-Aug-2026",
+            status: "Pending",
+            priority: "Low",
+        },
+    ],
+};
+export function reducer(state, action) {
+    switch (action.type) {
+        case "ROUTE_CHANGED":
+            return {
+                ...state,
+                route: action.payload,
+            };
+        case "ADD_TASK": {
+            const updatedTasks = [
+                ...state.tasks,
+                {
+                    id: state.tasks.length,
+                    ...action.payload,
+                },
+            ];
+            return { ...state, tasks: updatedTasks };
+        }
+        case "UPDATE_TASK": {
+            const { id, updates } = action.payload;
+
+            const updatedTasks = state.tasks.map((task) =>
+                task.id == id ? { ...task, ...updates } : task
+            );
+
+            return {
+                ...state,
+                tasks: updatedTasks,
+            };
+        }
+        case "DELETE_TASK": {
+            const { id } = action.payload;
+            const updatedTasks = state.tasks.filter((task) => task.id != id);
+            return {
+                ...state,
+                tasks: updatedTasks,
+            };
+        }
+        default:
+            return state;
+    }
+}
+
+export function createStore(initialState, reducer) {
+    let state = initialState;
+    const listeners = [];
+    return {
+        getState() {
+            return state;
+        },
+        dispatch(action) {
+            state = reducer(state, action);
+            listeners.forEach((listener) => listener(state));
+        },
+        subscribe(listener) {
+            listeners.push(listener);
+            return () => {
+                const index = listener.indexOf(listener);
+                if (index > -1) listener.splice(index, 1);
+            };
+        },
+    };
+}
+
+export const store = createStore(initialState, reducer);
+
+export function getStats(tasks) {
+    const total = tasks.length;
+    const completed = tasks.filter(
+        (task) => task.status === "Completed"
+    ).length;
+    return { total, completed, pending: total - completed };
+}
---
+++ js/week2/day5/src/router.js
@@ -0,0 +1,94 @@
+// router.js
+import { store } from "./js/store.js";
+import home from "./routes/home.js";
+import Login from "./routes/login.js";
+
+const routes = {
+    "/": home,
+    login: Login,
+    404: () => "<h1>404 Page Not Found </h1>",
+};
+
+const app = document.getElementById("app");
+
+const render = (state) => {
+    const viewFunction = routes[state.route.path] || routes["404"];
+    app.innerHTML = viewFunction();
+};
+
+window.addEventListener("click", (event) => {
+    const element = event.target.closest("[data-action");
+    if (!element) return;
+
+    const action = element.dataset.action;
+    const taskId = element.closest("[data-id]")?.dataset.id;
+    if (action === "delete-task") {
+        //remove task
+    }
+    if (action === "open-modal") {
+        const modal = document.querySelector(".task-modal");
+        modal.style.display = "flex";
+    }
+    if (action === "close-modal") {
+        const modal = document.querySelector(".task-modal");
+        modal.style.display = "none";
+    }
+    if (action === "complete") {
+        const state = store.getState();
+        let taskList = state.tasks;
+
+        const status =
+            taskList[taskId].status == "Pending" ? "Completed" : "Pending";
+
+        store.dispatch({
+            type: "UPDATE_TASK",
+            payload: {
+                id: taskId,
+                updates: {
+                    status: status,
+                },
+            },
+        });
+    }
+
+    if (action === "remove-task") {
+        store.dispatch({
+            type: "DELETE_TASK",
+            payload: {
+                id: taskId,
+            },
+        });
+    }
+    if (action === "submit-task") {
+        event.preventDefault();
+        const form = document.getElementById("task-form");
+        const data = new FormData(form);
+        const title = data.get("name");
+        const priority = data.get("priority");
+        const assignee = data.get("assignee");
+        const due = data.get("due-date");
+
+        store.dispatch({
+            type: "ADD_TASK",
+            payload: {
+                title: title,
+                assigned: assignee,
+                due: due,
+                status: "Pending",
+                priority: priority,
+            },
+        });
+    }
+});
+
+const syncRouterWithHash = () => {
+    const path = location.hash.slice(1) || "/";
+    store.dispatch({
+        type: "ROUTE_CHANGED",
+        payload: { path, params: {} },
+    });
+};
+store.subscribe(render);
+
+window.addEventListener("load", syncRouterWithHash);
+window.addEventListener("hashchange", syncRouterWithHash);
